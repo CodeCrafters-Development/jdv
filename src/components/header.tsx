d1aa13b8821670256d8053cb670294244bd114fd
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-04-28 23:37:49 +0530

    sample commit for image

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/assets/jdv-logo.png" alt="JDV" height={100} width={100} />
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
