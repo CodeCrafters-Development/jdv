3ba4ceba28729ffbd048da9380a53b9da6113cad
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-05-06 01:21:25 +0530

    Added new fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,18 @@ const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      NeueMontreal: ["NeueMontreal-Light", "sans"],
+      NeueMontrealLight: ["NeueMontreal-Light", "sans"],
+      NeueMontrealMedium: ["NeueMontreal-Medium", "sans"],
+      NeueMontrealBold: ["NeueMontreal-Bold", "sans"],
       PlayfairDisplay: ["Playfair Display", "sans"],
     },
     extend: {
       colors: {
         primary: "var(--primary)",
         secondary: "var(--secondary)",
+      },
+      backgroundImage: {
+        "hero-backdrop": "url('/assets/images/hero-backdrop.png')",
       },
     },
   },
