b47016cb978e467d193f7cd182e15f50b773f8c3
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-05-05 23:20:44 +0530

    Updated hero section added intro videos

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,33 +1,36 @@
-import Footer from "../components/footer";
-import Header from "../components/header";
-
 const Home = () => {
   return (
     <main className="">
-      <Header />
       <section className="h-screen px-20">
         <div className="h-[100px]"></div>
-        <div className="bg-primary w-full h-[80%]">
-          <div className="h-full w-[40%] flex items-start justify-center flex-col gap-y-4">
-            <h1 className=" mx-auto font-PlayfairDisplay text-7xl font-medium">
-              Joy of Living .
-            </h1>
-            <p className="w-[80%] mx-auto">
-              Lorem ipsum dolor sit amet consectetur. Blandit fermentum posuere
-              volutpat in lorem purus rhoncus laoreet sapien. Placerat at
-              turpis.
-            </p>
-            <div className="w-[80%] mx-auto mt-3">
-              <a href="/">
-                <span className="bg-secondary text-primary px-10 py-3">
-                  View Collection
-                </span>
-              </a>
+        <div className="bg-primary w-full h-[80%] flex">
+          <div className="h-full flex-1 flex items-start justify-center flex-col gap-y-4">
+            <div className="mx-auto w-[500px] h-[50%] flex flex-col items-start justify-evenly">
+              <h1 className="font-PlayfairDisplay text-7xl font-medium">
+                Joy of Living .
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Blandit fermentum
+                posuere volutpat in lorem purus rhoncus laoreet sapien. Placerat
+                at turpis.
+              </p>
+              <div>
+                <a href="/">
+                  <span className="bg-secondary text-primary px-10 py-3">
+                    View Collection
+                  </span>
+                </a>
+              </div>
             </div>
           </div>
+          <video autoPlay muted loop className="grayscale">
+            <source src="/assets/videos/left.mp4" />
+          </video>
+          <video autoPlay muted loop className="grayscale">
+            <source src="/assets/videos/right.mp4" />
+          </video>
         </div>
       </section>
-      <Footer />
     </main>
   );
 };
