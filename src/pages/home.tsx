80fca004dbbdf151922028f16a0524ce18d85a4b
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-05-12 02:54:40 +0530

    fully responsive banner section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,31 +1,41 @@
 const Home = () => {
   return (
     <main>
-      <section className="h-screen px-0">
-        <div className="h-[60px]"></div>
-        <div className="w-full h-full flex items-start justify-center z-0">
-          <div className="group absolute z-10 h-full w-[80%] flex items-center justify-center flex-col gap-y-10">
-            <h1 className="font-PlayfairDisplay text-h1Mobile md:text-h1Md font-semibold text-primary mix-blend-difference">
-              Joy of Living
-            </h1>
-            <p className="font-semibold text-center text-primary">
-              Lorem ipsum dolor sit amet consectetur. Blandit fermentum posuere
-              volutpat in lorem purus rhoncus laoreet sapien. Placerat at
-              turpis.
-            </p>
-            <div>
-              <a href="/">
-                <span className="bg-primary text-secondary font-NeueMontrealMedium px-10 py-3">
-                  View Collection
-                </span>
-              </a>
+      <section className="h-screen px-0 lg:px-20">
+        <div className="h-[60px] xl:h-[100px]"></div>
+        <div className="w-full h-full flex max-xl:items-start max-xl:justify-center max-xl:z-0 xl:bg-primary xl:h-[80%]">
+          <div className="max-xl:group max-xl:absolute max-xl:z-10 h-[80%] xl:h-full w-[80%] flex items-center xl:items-start justify-center flex-col gap-y-10 xl:gap-y-4">
+            <div className="xl:mx-auto md:w-[50%] xl:w-[500px] h-fit xl:h-[50%] flex flex-col items-center gap-y-4 md:gap-y-5 xl:gap-y-0 xl:items-start xl:justify-evenly xl:bg-hero-backdrop">
+              <h1 className="font-PlayfairDisplay text-h1Mobile xl:text-h1Md font-semibold text-primary xl:text-secondary">
+                Joy of Living
+              </h1>
+              <p className="max-xl:font-semibold max-xl:text-center text-primary xl:text-secondary">
+                Lorem ipsum dolor sit amet consectetur. Blandit fermentum
+                posuere volutpat in lorem purus rhoncus laoreet sapien. Placerat
+                at turpis.
+              </p>
+              <div>
+                <a href="/">
+                  <span className="bg-primary text-secondary xl:bg-secondary xl:text-primary font-NeueMontrealMedium px-10 py-3">
+                    View Collection
+                  </span>
+                </a>
+              </div>
             </div>
           </div>
           <video
             autoPlay
             muted
             loop
-            className="group relative grayscale brightness-50 h-[85vh] rounded-md flex items-center justify-center"
+            className="grayscale max-xl:h-[85vh] max-xl:brightness-50 max-md:hidden"
+          >
+            <source src="/assets/videos/left.mp4" />
+          </video>
+          <video
+            autoPlay
+            muted
+            loop
+            className="max-xl:group max-xl:relative grayscale max-xl:brightness-50 max-xl:h-[85vh] max-xl:rounded-md max-xl:flex max-xl:items-center max-xl:justify-center"
           >
             <source src="/assets/videos/right.mp4" />
           </video>
