d1a50ed551dc264989c7a4d0d13d2bd27a7df1dc
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-05-12 02:08:19 +0530

    fully responsive footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,9 @@ import { LuFacebook, LuInstagram, LuMail, LuPhone } from "react-icons/lu";
 
 const Footer = () => {
   return (
-    <footer className="bg-secondary text-white py-10">
+    <footer className="bg-secondary text-white flex flex-col py-10 gap-y-10">
       <div className="w-full">
-        <div className="w-[25%] mx-auto flex items-center justify-center flex-col gap-y-4">
+        <div className="w-[50%] md:w-[25%] mx-auto flex items-center justify-center flex-col gap-y-4 ">
           <h3 className="font-PlayfairDisplay text-4xl">Joie De Vire</h3>
           <hr className="bg-white/20 w-full h-[1px]" />
           <div className="w-full flex items-center justify-center gap-x-4">
@@ -23,12 +23,24 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="py-5 flex items-center justify-between px-10 text-sm">
+      <div className="flex items-start justify-between flex-col md:flex-row gap-y-2 px-5 md:px-20 text-sm">
         <p className="text-xs">© 2024 All Rights Reserved</p>
-        <ul className="flex items-center justify-center gap-x-10">
-          <li>Privacy Policy</li>
-          <li>Terms of Use</li>
-          <li>Sales and Refunds</li>
+        <ul className="flex items-start justify-center gap-x-1">
+          <li>
+            <a href="/">
+              <span className="hover:underline">Privacy Policy</span> •
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <span className="hover:underline">Terms of Use</span> •
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <span className="hover:underline">Sales and Refunds</span>
+            </a>
+          </li>
         </ul>
       </div>
     </footer>
