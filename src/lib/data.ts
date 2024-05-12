096a3b43312c4a401bec5e26c16b39194a73bb36
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-05-12 17:00:31 +0530

    Added mens and women products sample

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -14,7 +14,7 @@ export const navlinks: NavigationLinkTypes[] = [
   { name: "Kids", href: "/kids", hasSub: false },
 ];
 
-export const products: ProductType[] = [
+export const productsMen: ProductType[] = [
   {
     id: "JDV 01",
     productName: "Black Leather with Chrome Handles",
@@ -85,6 +85,90 @@ export const products: ProductType[] = [
     ],
     images: [
       { imgUri: "/assets/images/recent3.jpeg", isMain: true },
+      {
+        imgUri: "/assets/images/sub1.jpeg",
+      },
+      {
+        imgUri: "/assets/images/sub2.jpeg",
+      },
+      {
+        imgUri: "/assets/images/sub3.jpeg",
+      },
+    ],
+  },
+];
+
+export const productsWomen: ProductType[] = [
+  {
+    id: "JDV 01",
+    productName: "Black Leather with Chrome Handles",
+    isInStock: "In Stock",
+    size: ["XS", "S", "M", "L", "XL", "XXL"],
+    price: 4000,
+    installment: [
+      {
+        id: 1,
+        name: "KOKO",
+        numOfInstallments: 3,
+        brandImg: "/assets/images/koko.png",
+      },
+    ],
+    images: [
+      { imgUri: "/assets/images/bag1.jpeg", isMain: true },
+      {
+        imgUri: "/assets/images/sub1.jpeg",
+      },
+      {
+        imgUri: "/assets/images/sub2.jpeg",
+      },
+      {
+        imgUri: "/assets/images/sub3.jpeg",
+      },
+    ],
+  },
+  {
+    id: "JDV 01",
+    productName: "Black Leather with Chrome Handles",
+    isInStock: "In Stock",
+    size: ["XS", "S", "M", "L", "XL", "XXL"],
+    price: 4000,
+    installment: [
+      {
+        id: 1,
+        name: "KOKO",
+        numOfInstallments: 3,
+        brandImg: "/assets/images/koko.png",
+      },
+    ],
+    images: [
+      { imgUri: "/assets/images/bag2.jpeg", isMain: true },
+      {
+        imgUri: "/assets/images/sub1.jpeg",
+      },
+      {
+        imgUri: "/assets/images/sub2.jpeg",
+      },
+      {
+        imgUri: "/assets/images/sub3.jpeg",
+      },
+    ],
+  },
+  {
+    id: "JDV 01",
+    productName: "Black Leather with Chrome Handles",
+    isInStock: "In Stock",
+    size: ["XS", "S", "M", "L", "XL", "XXL"],
+    price: 4000,
+    installment: [
+      {
+        id: 1,
+        name: "KOKO",
+        numOfInstallments: 3,
+        brandImg: "/assets/images/koko.png",
+      },
+    ],
+    images: [
+      { imgUri: "/assets/images/bag3.jpeg", isMain: true },
       {
         imgUri: "/assets/images/sub1.jpeg",
       },
