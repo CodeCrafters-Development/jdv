948e955ec45a4d3a887f6fe2cc5df47258690a43
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-05-25 19:00:35 +0530

    Added backdrop and other minor changes

--- src/components/up-coming.tsx
+++ src/components/up-coming.tsx
@@ -10,10 +10,14 @@ const UpComing = ({ title, description, link }: UpComingProps) => {
   return (
     <>
       <img src="/assets/images/jdv-logo.png" alt="JDV" height={70} width={70} />
-      <div className="bg-primary w-[95%] py-16">
-        <h1 className="font-PlayfairDisplay tracking-[-5px] md:tracking-[-8px] text-h1Mobile md:text-h1Desktop text-center">
+      <div className="relative z-10 bg-primary w-[95%] py-16 overflow-hidden">
+        <h1 className="font-PlayfairDisplay z-10 tracking-[-5px] md:tracking-[-8px] text-h1Mobile md:text-h1Desktop text-center">
           {title}
         </h1>
+        <img
+          src="/assets/images/hero-backdrop.png"
+          className="absolute z-20 w-full h-full left-0 bottom-0 object-cover mix-blend-color-dodge"
+        />
       </div>
       <p className="text-xs text-center">{description}</p>
       <div>
