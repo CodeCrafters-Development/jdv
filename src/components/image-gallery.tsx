5febd81dff4ed7530abef5d7290b36544f15d576
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-05-27 18:38:02 +0530

    Implemented product page gallery

---
+++ src/components/image-gallery.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { ImageData } from "../lib/types";
+
+type ImageGalleryProps = {
+  slug?: string; //TODO: use slug to get image data
+  images?: ImageData[];
+};
+
+const ImageGallery = ({ slug, images }: ImageGalleryProps) => {
+  const mainImage = images?.find((image) => image.isMain);
+  const [selectedImage, setSelectedImage] = useState(mainImage?.imgUri);
+
+  return (
+    <div className="w-full md:w-1/2 md:h-[598px] flex flex-col-reverse md:flex-row md:gap-x-3 p-1 gap-y-3">
+      <div className="flex items-center justify-center md:flex-col gap-x-3 md:gap-y-3">
+        {/* Images */}
+        {images?.map((item, index) => (
+          <React.Fragment key={index}>
+            <img
+              src={item.imgUri}
+              className="w-[114px] h-[190px] object-cover cursor-pointer"
+              onClick={() => setSelectedImage(item.imgUri)}
+            />
+          </React.Fragment>
+        ))}
+      </div>
+      <div className="flex-1 max-md:h-[50vh] ">
+        {selectedImage ? (
+          <img src={selectedImage} className="h-full w-full object-cover" />
+        ) : (
+          <> {/* TODO: Implement error image */} </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ImageGallery;
